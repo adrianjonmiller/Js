08682bad85c6f87e088e8b4ce3a6beb059a61b23
Adrian Miller <adrianjonmiller@gmail.com>
2018-08-06 16:07:33 -0700

    removed old code

--- _src/getTagName.js
+++
@@ -1,3 +0,0 @@
-export default function getTagName ($node) {
-  return $node.tagName ? $node.tagName.toLowerCase() : $node.nodeName;
-};
--- _src/styleNode.js
+++
@@ -1,9 +0,0 @@
-// Look into doing local and global styles maybe..?
-// Ability to apply a global class and a specific ID
-
-export default function () {
-  let styleNode = document.createElement('style');
-
-  styleNode.type = 'text/css';
-  return styleNode;
-};
